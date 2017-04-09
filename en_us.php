932e0886c48094ef373ffca5bee57caa7fdfb1da
GS-M <service@gs-m.org>
2017-04-09 13:09:12 +0800

    v1.1

---
+++ en_us.php
@@ -0,0 +1,41 @@
+<?php
+	$LAN['NAME'] = "English";
+	$LAN['FLAG'] = "us";
+	
+	$LAN['TITLE'] = "GS-M.ORG - Game Server Live Status, Banners, Usability monitoring";
+	$LAN['HOME'] = "Home";
+	$LAN['ADD_SERVER'] = "Add Server";
+	$LAN['FORUMS'] = "Forums";
+	
+	$LAN['SEARCH'] = "Search";
+	$LAN['NAME'] = "Name";
+	$LAN['SRVNAME'] = "Server Name";
+	$LAN['ALL_GAMES'] = "All Games";
+	$LAN['LOCATION'] = "Location";
+	$LAN['ALL_LOCATIONS'] = "All Locations";
+	$LAN['ADDITION_SETTINGS'] = "Addition Settings";
+	$LAN['NOT_FULL'] = "Not Full";
+	$LAN['NO_PASSWORD'] = "No Password";
+	$LAN['NOT_EMPTY'] = "Not Empty";
+	$LAN['GAME'] = "Game";
+	$LAN['PLAYERS'] = "Players";
+	$LAN['MAP'] = "Map";
+	$LAN['IPPORT'] = "IP:Port";
+	$LAN['IP'] = "IP";
+	$LAN['PORT'] = "Port";
+	$LAN['ADD_SERVER'] = "Add Server";
+	$LAN['SUBMIT'] = "Submit";
+	$LAN['ERROR_MSG'] = "Opps! Something went wrong.";
+	$LAN['ERROR_MSG_GAME'] = "Wrong Game Type.";
+	$LAN['ERROR_INVAILD_IP'] = "Invaild IP Address.";
+	$LAN['ERROR_INVAILD_PORT'] = "Invaild Port.";
+	$LAN['SUCCESS_MSG'] = "Success!";
+	$LAN['ERROR_ALREAD_EXIST'] = "This server is alreay exist!";
+	$LAN['ADD_SERVER_SUCCESS_MSG'] = "You have successfully added this server to GS-M! This server will be monitored, you can get the server status less than 5 minutes as the server is online.";
+	$LAN['SERVER_INFORMATION'] = "Server Informations";
+	$LAN['ERROR_SERVER_NOT_FOUND'] = "This server is not exist in our database";
+	$LAN['LAST_UPDATE'] = "Last Update(UTC)";
+	$LAN['SERVICES'] = "Services";
+	$LAN['HCOUNT'] = "HCount";
+	$LAN['HCOUNT_HELP'] = "In order to change the image's color, change &color=255,255,255 to what you like in RGB";
+?>
---
+++ zh_cn.php
@@ -0,0 +1,41 @@
+<?php
+	$LAN['NAME'] = "简体中文";
+	$LAN['FLAG'] = "cn";
+	
+	$LAN['TITLE'] = "GS-M.ORG - 游戏服务器实时状态, 横幅, 可用性检测";
+	$LAN['HOME'] = "主页";
+	$LAN['ADD_SERVER'] = "添加服务器";
+	$LAN['FORUMS'] = "论坛";
+	
+	$LAN['SEARCH'] = "搜索";
+	$LAN['NAME'] = "名称";
+	$LAN['SRVNAME'] = "服务器名称";
+	$LAN['ALL_GAMES'] = "所有游戏";
+	$LAN['LOCATION'] = "地点";
+	$LAN['ALL_LOCATIONS'] = "所有地点";
+	$LAN['ADDITION_SETTINGS'] = "附加设置";
+	$LAN['NOT_FULL'] = "没有满人";
+	$LAN['NO_PASSWORD'] = "没有密码";
+	$LAN['NOT_EMPTY'] = "不是没人";
+	$LAN['GAME'] = "游戏";
+	$LAN['PLAYERS'] = "玩家";
+	$LAN['MAP'] = "地图";
+	$LAN['IPPORT'] = "IP:端口";
+	$LAN['IP'] = "IP";
+	$LAN['PORT'] = "端口";
+	$LAN['ADD_SERVER'] = "添加服务器";
+	$LAN['SUBMIT'] = "提交";
+	$LAN['ERROR_MSG'] = "发生错误";
+	$LAN['ERROR_MSG_GAME'] = "提交的游戏类型错误.";
+	$LAN['ERROR_INVAILD_IP'] = "提交的IP地址错误.";
+	$LAN['ERROR_INVAILD_PORT'] = "提交的端口号错误.";
+	$LAN['SUCCESS_MSG'] = "成功!";
+	$LAN['ERROR_ALREAD_EXIST'] = "此服务器已经存在!";
+	$LAN['ADD_SERVER_SUCCESS_MSG'] = "成功添加此服务器到GS-M! 服务器现在开始将被监控, 如果此服务器在线, 你将在5分钟内获得服务器状态信息.";
+	$LAN['SERVER_INFORMATION'] = "服务器信息";
+	$LAN['ERROR_SERVER_NOT_FOUND'] = "此服务器不存在";
+	$LAN['LAST_UPDATE'] = "更新时间(UTC)";
+	$LAN['SERVICES'] = "服务";
+	$LAN['HCOUNT'] = "HCount";
+	$LAN['HCOUNT_HELP'] = "如果要修改图片颜色，修改&color=255,255,255到你想要的颜色(RGB模式)";
+?>
